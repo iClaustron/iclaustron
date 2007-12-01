4b778e7f885a96e03aa91cf88c1e68dbfa9a0614
mikael <mikael@763894fe-7313-0410-9b4e-9763914fbcec>
2007-12-01 23:30:11 +0000

    Added count lines script
    git-svn-id: svn+ssh://192.168.1.105/Users/mikael/icl-repository/trunk@148 763894fe-7313-0410-9b4e-9763914fbcec

---
+++ count_line.sh
@@ -0,0 +1,2 @@
+wc -l *.ac *.sh *.am */*.am api_clusterserver/*.[ch] api_dataserver/*.[ch] cluster_client/*.[ch] cluster_mgr/*.[yl] cluster_mgr/ic_clmgr.c cluster_server/*.[ch] comm/*.[ch] \
+  fileserver/*.[ch] pcntrl/*.[ch] port/*.[ch] repserver/*.[ch] test/*.[ch] util/*.[ch] include/hash*.h include/ic*.h
