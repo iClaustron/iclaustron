2f72926b1160c1789b225dde4f4f983fb8d8b4e4
mikael <mikael@763894fe-7313-0410-9b4e-9763914fbcec>
2008-12-02 21:30:02 +0000

    Added ic_dyn.h
    git-svn-id: svn+ssh://192.168.1.105/Users/mikael/icl-repository/trunk@334 763894fe-7313-0410-9b4e-9763914fbcec

---
+++ include/ic_dyn_array.h
@@ -0,0 +1,69 @@
+/* Copyright (C) 2007, 2008 iClaustron AB
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+
+#ifndef IC_DYNAMIC_ARRAY_H
+#define IC_DYNAMIC_ARRAY_H
+#include <ic_common.h>
+
+struct ic_dynamic_array;
+typedef struct ic_dynamic_array IC_DYNAMIC_ARRAY;
+
+struct ic_dynamic_array_ops
+{
+  int (*ic_insert_dynamic_array) (IC_DYNAMIC_ARRAY *dyn_array,
+                                  const gchar *buf, guint64 size);
+  int (*ic_write_dynamic_array) (IC_DYNAMIC_ARRAY *dyn_array,
+                                 guint64 position,
+                                 guint64 size,
+                                 const gchar *buf);
+  guint64 (*ic_get_current_size) (IC_DYNAMIC_ARRAY *dyn_array);
+  int (*ic_write_dynamic_array_to_disk) (IC_DYNAMIC_ARRAY *dyn_array,
+                                         int file_ptr);
+  int (*ic_read_dynamic_array) (IC_DYNAMIC_ARRAY *dyn_array,
+                                guint64 position, guint64 size,
+                                gchar *ret_buf);
+  void (*ic_free_dynamic_array) (IC_DYNAMIC_ARRAY *dyn_array);
+};
+typedef struct ic_dynamic_array_ops IC_DYNAMIC_ARRAY_OPS;
+
+struct ic_dynamic_array
+{
+  IC_DYNAMIC_ARRAY_OPS da_ops;
+};
+
+IC_DYNAMIC_ARRAY* ic_create_simple_dynamic_array();
+IC_DYNAMIC_ARRAY* ic_create_ordered_dynamic_array();
+
+struct ic_dynamic_translation;
+typedef struct ic_dynamic_translation IC_DYNAMIC_TRANSLATION;
+struct ic_dynamic_translation_ops
+{
+  int (*ic_insert_translation_object) (IC_DYNAMIC_TRANSLATION *dyn_trans,
+                                       guint64 *index,
+                                       void *object);
+  void (*ic_remove_translation_object) (IC_DYNAMIC_TRANSLATION *dyn_trans,
+                                        guint64 index,
+                                        void *object);
+  void (*ic_free_translation_object) (IC_DYNAMIC_TRANSLATION *dyn_trans);
+};
+typedef struct ic_dynamic_translation_ops IC_DYNAMIC_TRANSLATION_OPS;
+
+struct ic_dynamic_translation
+{
+  IC_DYNAMIC_TRANSLATION_OPS dt_ops;
+};
+
+IC_DYNAMIC_TRANSLATION* ic_create_translation_object();
+#endif
